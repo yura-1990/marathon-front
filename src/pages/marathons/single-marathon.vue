b619c92e4ef485edc3b5c3da12bb0f329ec8b07f
yura-1990 <yurayur1990@gmail.com>
2025-02-14 10:56:44 +0500

    info

--- src/pages/marathons/single-marathon.vue
+++ src/pages/marathons/single-marathon.vue
@@ -71,7 +71,7 @@ watch(()=>locale.value, async (language)=>{
               </div>
               <div class="page-title-heading">
                 <h2 class="title">{{ singleMarathons?.name }}</h2>
-                <p class="text-white " v-html="singleMarathons.description"></p>
+
                 <h4 class="text-white ">{{ singleMarathons?.price }} UZS</h4>
               </div>
             </div>
@@ -81,48 +81,18 @@ watch(()=>locale.value, async (language)=>{
     </div>
   </div>
   <div class="site-main">
-
     <!-- prt-blog-single -->
-<!--    <div class="prt-row blog-details-section clearfix">-->
-<!--      <div class="container">-->
-<!--        <div class="row align-items-center">-->
-<!--          <div class="col-lg-12">-->
-<!--            <div class="prt-page-title-row-heading">-->
-<!--              <div class="prt-page-title-link d-flex align-items-center">-->
-<!--                <RouterLink :to="`/participate/${marathon?.marathon.id}`" v-if="marathon?.marathon?.event_has_marathon?.status" class="prt-btn border-0 prt-btn-size-md prt-btn-shape-rounded prt-btn-style-fill prt-btn-color-skincolor me-3">-->
-<!--                  {{ $t('participate') }}-->
-<!--                </RouterLink>-->
-
-<!--                <div class="post-category d-flex flex-wrap align-items-center">-->
-<!--                  <span>-->
-<!--                    <i class="ti ti-alarm-clock" aria-hidden="true"></i>-->
-<!--                  </span>-->
-<!--                  <div>-->
-<!--                    <span>-->
-<!--                      {{ settingStore.formatDate(marathon?.marathon?.event_has_marathon?.date_event) }}-->
-<!--                    </span>-->
-<!--                    <span>{{ marathon?.marathon?.datetime_from }} - {{ marathon?.marathon?.datetime_to }}</span>-->
-<!--                  </div>-->
-<!--                </div>-->
-<!--                <div class="post-date">-->
-<!--                  <i class="fa-light fa-user"></i>-->
-<!--                  <span> {{ marathon?.marathon?.participants ? marathon?.marathon.participants.length : 0 }} {{ $t('participants') }}</span>-->
-<!--                </div>-->
-<!--              </div>-->
-<!--              <div class="prt-page-title-details">-->
-
-<!--              </div>-->
-<!--              <div class="page-title-heading">-->
-<!--                <h2 class="title">{{ marathon?.marathon?.name }}</h2>-->
-<!--                <p class="text-white ">{{ marathon?.marathon?.description }}</p>-->
-<!--                <h4 class="text-white ">{{ marathon?.marathon?.price }} UZS</h4>-->
-<!--              </div>-->
-<!--            </div>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--    </div>-->
-    <!-- prt-blog-single end-->
+    <div class="pt-40  blog-details-section clearfix">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-12">
+            <div class="prt-page-title-row-heading">
+                <p class="text-white " v-html="singleMarathons.description"></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
   </div>
 </div>
